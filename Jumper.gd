6494a23a5376489cb80361dfb165ffbccd3f90e7
marivgil <gil.maricruz@gmail.com>
2018-09-09 18:29:14 -0300

    el player se mueve hacia los lados

--- Jumper.gd
+++ Jumper.gd
@@ -1,13 +1,22 @@
 extends RigidBody2D
 
 var jump_speed = 600
+var speed = 300
 
 func _ready():
-
+	#_fixed_process(true)
 	pass
 
+func _process(delta):
+	var left_key = Input.is_action_pressed('ui_left')
+	var right_key = Input.is_action_pressed('ui_right')
 
+	if left_key:
+		set_linear_velocity(Vector2(-speed, get_linear_velocity().y))
+	if right_key:
+		set_linear_velocity(Vector2(speed, get_linear_velocity().y))
+		
 func collision(body):
 	if body.is_in_group('paddles'):
 		set_linear_velocity(Vector2(0,-jump_speed))
-	pass # replace with function body
+	pass
