610f6efe5ab7644a83901d97065bc8deabb5339d
marivgil <gil.maricruz@gmail.com>
2018-09-09 18:02:50 -0300

    el player colisiona contra la plataforma

--- Jumper.gd
+++ Jumper.gd
@@ -1,5 +1,13 @@
 extends RigidBody2D
 
+var jump_speed = 600
+
 func _ready():
 
 	pass
+
+
+func collision(body):
+	if body.is_in_group('paddles'):
+		set_linear_velocity(Vector2(0,-jump_speed))
+	pass # replace with function body
