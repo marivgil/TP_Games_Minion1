a9bba7e3c1044075f8b1e37b3c049cdf104c8a1e
marivgil <gil.maricruz@gmail.com>
2018-09-09 17:36:47 -0300

    el bicho cae, existe plataforma, fondo y camara seguún tutorial

--- Jumper.gd
+++ Jumper.gd
@@ -1,17 +1,5 @@
 extends RigidBody2D
 
-const GRAVITY = 200
-const WALK_SPEED = 200
-var velocity = Vector2()
+func _ready():
 
-func _physics_process(delta):
-    velocity.y += delta * GRAVITY
-
-    if Input.is_action_pressed("ui_left"):
-        velocity.x = -WALK_SPEED
-    elif Input.is_action_pressed("ui_right"):
-        velocity.x =  WALK_SPEED
-    else:
-        velocity.x = 0
-#func collition(body):
-#	velocity.bounce(5)
+	pass
